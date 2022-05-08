7acb3247821b6e7f52ae2d09ba86b52c9b0edd2a
Daniel Weiner <daweiner16@gmail.com>
2022-05-07 21:05:10 -0400

    add stub code for related items container component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {createRoot} from 'react-dom/client';
+import RelatedItems from './relatedItems/relatedItems.jsx'
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+
     }
   }
   render() {
     return(
-      <div>
-        This is a test message.
-      </div>
+      <RelatedItems />
     )
   }
 }
---
+++ client/src/relatedItems/relatedItems.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class RelatedItems extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>Hello from related</div>
+    )
+  }
+}
+
+export default RelatedItems
+
