b6a2ea12c29ae070df8f7a6cd6b941bcbe40f2c5
Daniel Weiner <daweiner16@gmail.com>
2022-05-10 13:46:43 -0400

    convert carousel to class component

--- client/src/relatedItems/Carousel.jsx
+++ client/src/relatedItems/Carousel.jsx
@@ -1,26 +1,36 @@
-import React from 'react'
-import '../carousel.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../carousel.css';
 
-const Carousel = (props) => {
-  const {children} = props
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
 
-  return (
-      <div className="carousel-container">
-          <div className="carousel-wrapper">
-            <button className="left-arrow">
-                &lt;
-            </button>
-              <div className="carousel-content-wrapper">
-                  <div className="carousel-content">
-                      {children}
-                  </div>
-              </div>
-            <button className="right-arrow">
-                &gt;
-            </button>
-          </div>
-      </div>
-  )
+    }
+  }
+
+  render() {
+
+    return (
+        <div className="carousel-container">
+            <div className="carousel-wrapper">
+              <button className="left-arrow">
+                  &lt;
+              </button>
+                <div className="carousel-content-wrapper">
+                    <div className="carousel-content">
+                        {this.props.children}
+                    </div>
+                </div>
+              <button className="right-arrow">
+                  &gt;
+              </button>
+            </div>
+        </div>
+    )
+  }
+
 }
 
 export default Carousel
