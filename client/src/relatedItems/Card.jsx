323babaa39a758198cdb3258fd23922bc8a569ef
Daniel Weiner <daweiner16@gmail.com>
2022-05-13 15:21:38 -0400

    only display modals for related cards

--- client/src/relatedItems/Card.jsx
+++ client/src/relatedItems/Card.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import '../carousel.css'
 
-const Card = ({img, type}) => {
+const Card = ({img, type, isRelated}) => {
   return (
     <div className={`card ${type}`}>
-      <div className="modal">
+      { isRelated &&
 
-      </div>
+        <div className="modal">
+
+        </div>
+      }
+
       <img src="https://via.placeholder.com/150" alt="Avatar" ></img>
       <div className="container">
         <h4><b>John Doe</b></h4>
