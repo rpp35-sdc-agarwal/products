33f7c4b93178a44843b36ef03eebedcd61f16181
Daniel Weiner <daweiner16@gmail.com>
2022-05-18 13:17:44 -0400

    create StarRating functional component that displays rating in card component as a number

--- client/src/relatedItems/Card.jsx
+++ client/src/relatedItems/Card.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../carousel.css'
+import StarRating from './StarRating.jsx'
 
 
 const Card = ({product, type, isRelated, rating}) => {
@@ -17,6 +18,7 @@ const Card = ({product, type, isRelated, rating}) => {
         <h4><b>{product.category}</b></h4>
         <p>{product.name}</p>
       </div>
+      <StarRating rating={rating} />
 
     </div>
 
---
+++ client/src/relatedItems/StarRating.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const StarRating = ({rating}) => (
+  <h4>{rating}</h4>
+)
+
+
+
+export default StarRating;
