6fd92d6463afee0515dd035d41dc21ec88f9a069
Daniel Weiner <daweiner16@gmail.com>
2022-05-19 11:58:54 -0400

    update route to use middleware

--- server/index.js
+++ server/index.js
@@ -9,6 +9,9 @@ app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+const getRelatedProducts = require('./middleware/relatedProducts.js')
+
+
 
 const PORT = process.env.PORT || 3000;
 
@@ -31,39 +34,7 @@ app.get('/products', (req, res) => {
   .catch(err => console.log('there was an error'))
 })
 
-app.get('/products/:product_id/related', (req, res) => {
-  var id = req.params.product_id
-  axios(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${id}/related`, {
-    headers: {
-      'Authorization': API
-    }
-  })
-  .then(response => {
-    var promises = [];
-    for (var i = 0; i < response.data.length; i++) {
-      var id = response.data[i]
-      promises.push(axios(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${id}`, {
-        headers: {
-          'Authorization': API
-        }
-      }))
-    }
-    return promises
-
-  })
-  .then(promises => {
-    return Promise.all(promises)
-  })
-  .then(results => {
-    var data = [];
-    for (var i = 0; i < results.length; i++) {
-      data.push(results[i].data);
-    }
-    return data;
-  })
-  .then(data => res.json(data))
-
-  .catch(err => console.log('there was an error'))
-
+app.get('/products/:product_id/related', getRelatedProducts, (req, res) => {
+  res.json(res.products);
 
 })
---
+++ server/middleware/relatedProducts.js
@@ -0,0 +1,53 @@
+const axios = require('axios');
+const API = require('../../config.example.js')
+
+
+const getRelatedProducts = (req, res, next) => {
+  var id = req.params.product_id
+  console.log('what is id', id)
+
+  axios(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${id}/related`, {
+    headers: {
+      'Authorization': API
+    }
+  })
+  .then(response => {
+
+    var promises = [];
+    for (var i = 0; i < response.data.length; i++) {
+      var id = response.data[i]
+      promises.push(axios(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${id}`, {
+        headers: {
+          'Authorization': API
+        }
+      }))
+    }
+
+    return promises
+
+  })
+  .then(promises => {
+
+    return Promise.all(promises)
+  })
+  .then(results => {
+    var data = [];
+    for (var i = 0; i < results.length; i++) {
+      data.push(results[i].data);
+    }
+
+    return data;
+  })
+  .then(data => {
+    res.products = data
+    next();
+
+  })
+
+  .catch(err => console.log('there was an error'))
+
+
+
+}
+
+module.exports = getRelatedProducts;
