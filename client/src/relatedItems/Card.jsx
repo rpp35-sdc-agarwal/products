bb46c298b613fedce58d972fa886256940fa1f24
Daniel Weiner <daweiner16@gmail.com>
2022-05-20 10:31:44 -0400

    display sale price next to default price, if product on sale

--- client/src/relatedItems/Card.jsx
+++ client/src/relatedItems/Card.jsx
@@ -2,9 +2,18 @@ import React from 'react';
 // import '../carousel.css'
 import StarRating from './StarRating.jsx'
 import DefaultPrice from './DefaultPrice.jsx'
+import SalePrice from './SalePrice.jsx'
 
 
 const Card = ({product, type, isRelated, rating}) => {
+  var price = null;
+  var salePrice = product.sale_price
+  if (salePrice) {
+    price = <SalePrice salePrice={salePrice}
+            price={product.default_price} />
+  } else {
+    price = <DefaultPrice price={product.default_price} />
+  }
   return (
     <div className={`card ${type}`}>
       { isRelated &&
@@ -18,7 +27,7 @@ const Card = ({product, type, isRelated, rating}) => {
       <div className="container">
         <h4><b>{product.category}</b></h4>
         <p>{product.name}</p>
-        <DefaultPrice price={product.default_price} />
+        {price}
 
       </div>
       <StarRating rating={rating} />
---
+++ client/src/relatedItems/SalePrice.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SalePrice = ({salePrice, price}) => {
+  return (
+    <>
+    <span>{salePrice} </span>
+    <span>{price}</span>
+    </>
+  )
+}
+
+export default SalePrice;
