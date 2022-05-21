b0fc271ea3f051e19845051900f04eb2be570c8d
Kiki Cai <92818058+caixiuqi2013@users.noreply.github.com>
2022-05-21 10:45:52 -0700

    Revert "Update price"

--- server/index.js
+++ server/index.js
@@ -10,11 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 const getRelated = require('./middleware/relatedProducts.js')
-const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
-});
 /////////////////////////////////////////////////////////
 //    Products Routes
 /////////////////////////////////////////////////////////
@@ -51,11 +47,43 @@ app.get('/products', (req, res) => {
 /////////////////////////////////////////////////////////
 //    Related Items Routes
 /////////////////////////////////////////////////////////
+app.get('/products/:product_id/related', (req, res) => {
+  var id = req.params.product_id
+  axios(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${id}/related`, {
+    headers: {
+      'Authorization': API
+    }
+  })
+  .then(response => {
+    var promises = [];
+    for (var i = 0; i < response.data.length; i++) {
+      var id = response.data[i]
+      promises.push(axios(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${id}`, {
+        headers: {
+          'Authorization': API
+        }
+      }))
+    }
+    return promises
 
-app.get('/products/:product_id/related', [getRelated.getRelatedProducts, getRelated.getRelatedStyles, getRelated.addPriceToProducts], (req, res) => {
-  res.json(res.products)
+  })
+  .then(promises => {
+    return Promise.all(promises)
+  })
+  .then(results => {
+    var data = [];
+    for (var i = 0; i < results.length; i++) {
+      data.push(results[i].data);
+    }
+    return data;
+  })
+  .then(data => res.json(data))
+
+  .catch(err => console.log('there was an error'))
+
 
 })
+
 /////////////////////////////////////////////////////////
 //    Questions and Answers Routes
 /////////////////////////////////////////////////////////
@@ -90,7 +118,28 @@ app.get('/products/:product_id/related', [getRelated.getRelatedProducts, getRela
 // TODO: Add routes for Reviews
 
 
+const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
 
+app.get('/products', (req, res) => {
 
+  axios(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products`, {
+    headers: {
+      'Authorization': API
+    }
+  })
+  .then(response => {
 
+    res.json(response.data)
+
+  })
+  .catch(err => console.log('there was an error'))
+})
+
+app.get('/products/:product_id/related', [getRelated.getRelatedProducts, getRelated.getRelatedStyles, getRelated.addPriceToProducts], (req, res) => {
+  res.json(res.products)
+
+})
