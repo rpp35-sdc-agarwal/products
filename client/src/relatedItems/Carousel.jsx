630fcae43c45bccf583cd0d7ed5c7bfc07e6024b
Daniel Weiner <daweiner16@gmail.com>
2022-05-25 17:01:43 -0400

    on card click, make api call to update modal card in carousel state

--- client/src/relatedItems/Carousel.jsx
+++ client/src/relatedItems/Carousel.jsx
@@ -10,7 +10,9 @@ class Carousel extends React.Component {
     super(props)
     this.state = {
       currentIndex: 0,
-      products: this.props.products
+      products: this.props.products,
+      overviewProduct: null,
+      modalProduct: null,
 
 
     }
@@ -25,7 +27,9 @@ class Carousel extends React.Component {
     axios.get(`/products/71699/related`)
     .then(res => {
       this.setState({
-       products: [...res.data]
+       products: [...res.data],
+       modalProduct: res.data[0],
+       overviewProduct: res.data[0]
       })
     })
 
@@ -47,6 +51,12 @@ class Carousel extends React.Component {
 
   handleGetCard(id) {
     console.log('what is id in carousel', id)
+    axios.get(`/products/${id}`)
+    .then(res => {
+      this.setState({
+        modalProduct: res.data
+      })
+    })
   }
 
 
