0942b87a41a422a5e147eb3c1d057daa274f8391
Enjie Xu <xuenjie99@gmail.com>
2022-05-29 21:19:52 -0700

    made arrows hide

--- client/src/components/ProductOverview/GalleryThumbnails.jsx
+++ client/src/components/ProductOverview/GalleryThumbnails.jsx
@@ -5,28 +5,57 @@ class GalleryThumbnails extends React.Component {
     super(props);
     this.state = {
       currentStyle: this.props.currentStyle,
-      currentImageIndex: this.props.currentImageIndex
+      currentImageIndex: this.props.currentImageIndex,
+      thumbnailFirstIndex: this.props.thumbnailFirstIndex,
+      thumbnailLastIndex: this.props.thumbnailLastIndex,
     }
     this.handleImageClick = this.handleImageClick.bind(this);
     this.getClassName = this.getClassName.bind(this);
+    this.getUpArrowClassName = this.getUpArrowClassName.bind(this);
+    this.getDownArrowClassName = this.getDownArrowClassName.bind(this);
   }
 
   handleImageClick(e) {
-    this.props.handleImageChange(e.target[Object.keys(e.target)[1]].value)
+    let url = e.target[Object.keys(e.target)[1]].value;
+    let thumbnails = this.state.currentStyle.photos;
+    for(let i = 0; i < thumbnails.length; i++) {
+      if (thumbnails[i].thumbnail_url === url) {
+        this.props.handleImageChange(i);
+      }
+    }
   }
 
   UNSAFE_componentWillReceiveProps(props) {
     this.setState({
       currentStyle: props.currentStyle,
-      currentImageIndex: props.currentImageIndex
+      currentImageIndex: props.currentImageIndex,
+      thumbnailFirstIndex: props.thumbnailFirstIndex,
+      thumbnailLastIndex: props.thumbnailLastIndex
     })
   }
 
-  getClassName(index) {
-    if (index === this.state.currentImageIndex) {
+  getClassName(url) {
+    if (url === this.state.currentStyle.photos[this.state.currentImageIndex].thumbnail_url) {
       return 'seletedCarouselFrame';
     } else {
       return 'galleryCarouselFrame';
+    }
+  }
+
+  getUpArrowClassName() {
+    if (this.state.thumbnailFirstIndex === 0) {
+      return 'hide';
+    } else {
+      return '';
+    }
+  }
+
+  getDownArrowClassName() {
+    let lastIndex = this.state.currentStyle.photos.length;
+    if (this.state.thumbnailLastIndex === lastIndex) {
+      return 'hide';
+    } else {
+      return '';
     }
   }
 
@@ -34,17 +63,17 @@ class GalleryThumbnails extends React.Component {
     return(
       <div id = 'galleryCarouselDiv'>
         <div className='arrowDiv'>
-          <i className="arrow up"></i>
+          <i className="arrow up" id={this.getUpArrowClassName()} onClick={this.props.handleUpArrowClick}></i>
         </div>
-        {this.state.currentStyle.photos.map((photo, index) =>
+        {this.state.currentStyle.photos.slice(this.state.thumbnailFirstIndex, this.state.thumbnailLastIndex).map((photo, index) =>
           <div className='galleryCarouselImageFrame' key={index} onClick={this.handleImageClick}>
-            <img className={this.getClassName(index)}
-              src = {photo.thumbnail_url} value = {index}
+            <img className={this.getClassName(photo.thumbnail_url)}
+              src = {photo.thumbnail_url} value = {photo.thumbnail_url}
             />
           </div>
         )}
         <div className='arrowDiv'>
-          <i className="arrow down"></i>
+          <i className="arrow down" id={this.getDownArrowClassName()} onClick={this.props.handleDownArrowClick}></i>
         </div>
       </div>
     )
--- client/src/components/ProductOverview/ImageGallery.jsx
+++ client/src/components/ProductOverview/ImageGallery.jsx
@@ -6,12 +6,38 @@ class ImageGallery extends React.Component {
     super(props);
     this.state = {
       currentStyle: this.props.currentStyle,
-      currentImageIndex: 0
+      currentImageIndex: 0,
+      thumbnailFirstIndex: 0,
+      thumbnailLastIndex: 5
     }
     this.handleImageChange = this.handleImageChange.bind(this);
     this.handleRLClick = this.handleRLClick.bind(this);
+    this.handleDownArrowClick = this.handleDownArrowClick.bind(this);
+    this.handleUpArrowClick = this.handleUpArrowClick.bind(this);
+    this.renderImageDiv = this.renderImageDiv.bind(this);
   }
 
+  handleDownArrowClick() {
+    if (this.state.thumbnailLastIndex === this.state.currentStyle.photos.length) {
+      return;
+    } else {
+      this.setState({
+        thumbnailFirstIndex: this.state.thumbnailFirstIndex += 1,
+        thumbnailLastIndex: this.state.thumbnailLastIndex += 1
+      })
+    }
+  }
+
+  handleUpArrowClick() {
+    if (this.state.thumbnailFirstIndex === 0) {
+      return;
+    } else {
+      this.setState({
+        thumbnailFirstIndex: this.state.thumbnailFirstIndex -= 1,
+        thumbnailLastIndex: this.state.thumbnailLastIndex -= 1
+      })
+    }
+  }
 
   handleImageChange(urlIndex) {
     if(this.state.currentImageIndex === urlIndex) {
@@ -27,22 +53,31 @@ class ImageGallery extends React.Component {
     let photosLength = this.state.currentStyle.photos.length;
     if (e.target.value === 'next') {
       if(currentImageIndex === photosLength - 1) {
-        currentImageIndex = 0;
+        return;
       } else {
         currentImageIndex++;
+      }
+      let lastIndex = this.state.thumbnailLastIndex;
+      if(currentImageIndex === lastIndex) {
+        this.handleDownArrowClick();
       }
       this.setState({
         currentImageIndex: currentImageIndex
       })
     } else {
       if(currentImageIndex === 0) {
-        currentImageIndex = photosLength - 1;
+        return;
       } else {
         currentImageIndex--;
       }
+      let firstIndex = this.state.thumbnailFirstIndex;
       this.setState({
         currentImageIndex: currentImageIndex
       })
+      currentImageIndex++;
+      if(currentImageIndex === firstIndex) {
+        this.handleUpArrowClick();
+      }
     }
   }
 
@@ -53,23 +88,62 @@ class ImageGallery extends React.Component {
     })
   }
 
+  renderImageDiv() {
+    let index = this.state.currentImageIndex;
+    if(index === 0) {
+      return(
+        <div id = 'imageDiv'>
+        <div>
+          <button className='carousel-button next' value='next' onClick={this.handleRLClick}>&#8658;</button>
+        </div>
+        <img id = 'frame'
+          src = {this.state.currentStyle.photos[this.state.currentImageIndex].url}
+        />
+      </div>
+      )
+    } else if (index === this.state.currentStyle.photos.length - 1) {
+      return(
+        <div id = 'imageDiv'>
+        <div>
+          <button className='carousel-button prev' value='prev' onClick={this.handleRLClick}>&#8656;</button>
+        </div>
+        <img id = 'frame'
+          src = {this.state.currentStyle.photos[this.state.currentImageIndex].url}
+        />
+      </div>
+      )
+    } else {
+      return(
+        <div id = 'imageDiv'>
+        <div>
+          <button className='carousel-button prev' value='prev' onClick={this.handleRLClick}>&#8656;</button>
+        </div>
+        <div>
+          <button className='carousel-button next' value='next' onClick={this.handleRLClick}>&#8658;</button>
+        </div>
+        <img id = 'frame'
+          src = {this.state.currentStyle.photos[this.state.currentImageIndex].url}
+        />
+      </div>
+      )
+    }
+  }
+
   render() {
     return(
       <div id = 'galleryDiv' className = 'childDiv'>
 
-        <GalleryThumbnails currentStyle = {this.state.currentStyle} handleImageChange={this.handleImageChange} currentImageIndex={this.state.currentImageIndex}/>
+        <GalleryThumbnails
+          thumbnailFirstIndex={this.state.thumbnailFirstIndex}
+          thumbnailLastIndex={this.state.thumbnailLastIndex}
+          currentStyle = {this.state.currentStyle}
+          currentImageIndex={this.state.currentImageIndex}
+          handleDownArrowClick={this.handleDownArrowClick}
+          handleUpArrowClick={this.handleUpArrowClick}
+          handleImageChange={this.handleImageChange}
+        />
 
-        <div id = 'imageDiv'>
-          <div>
-            <button className='carousel-button prev' value='prev' onClick={this.handleRLClick}>&#8656;</button>
-          </div>
-          <div>
-            <button className='carousel-button next' value='next' onClick={this.handleRLClick}>&#8658;</button>
-          </div>
-          <img id = 'frame'
-            src = {this.state.currentStyle.photos[this.state.currentImageIndex].url}
-          />
-        </div>
+        {this.renderImageDiv()}
       </div>
     )
   }
