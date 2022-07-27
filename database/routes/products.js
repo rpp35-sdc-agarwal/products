f6e7bfbefc9374a86e6ec6e2402fe13acf05ef65
Kevin On <idealistkevin@gmail.com>
2022-07-26 21:09:16 -0700

    Further optimized database with redis

--- database/routes/products.js
+++ database/routes/products.js
@@ -1,26 +1,63 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../database.js')
+const db = require('../database.js');
+const { createClient } = require('redis');
 let ProductsService = require('../services/ProductsService.js');
-let StylesService = require('../services/StylesService.js')
+let StylesService = require('../services/StylesService.js');
+require('dotenv').config();
+
+const client = createClient();
+
+let start = async function () {
+  await client.connect();
+}
+
+start();
+
+const DEFAULT_EXPIRATION = 86400;
 
 // /product/
 router.get('/', async (req, res) => {
-  res.status(200).send(await ProductsService.getAllProducts());
+  let products = await getSetCache('products', async () => {
+    return await ProductsService.getAllProducts();
+  });
+  res.status(200).json(products);
 })
 
 router.get('/:product_id', async (req, res) => {
-  res.status(200).send(await ProductsService.getOneProduct(req.params.product_id));
+  let product = await getSetCache(`product${req.params.product_id}`, async () => {
+    return await ProductsService.getOneProduct(req.params.product_id);
+  })
+  res.status(200).json(product);
 })
 
 router.get('/:product_id/related', async (req, res) => {
-  res.status(200).send(await ProductsService.getRelated(req.params.product_id));
+  let related = await getSetCache(`related${req.params.product_id}`, async () => {
+    return await ProductsService.getOneProduct(req.params.product_id);
+  })
+  res.status(200).json(related);
 })
 
 router.get('/:product_id/styles', async (req, res) => {
-  let data = { product_id: req.params.product_id };
-  data.results = await StylesService.getProductStyle(req.params.product_id);
-  res.status(200).send(data);
+  let styles = await getSetCache(`styles${req.params.product_id}`, async () => {
+    let data = { product_id: req.params.product_id };
+    data.results = await StylesService.getProductStyle(req.params.product_id);
+    return data;
+  })
+  res.status(200).json(styles);
 })
+
+let getSetCache = async function (key, callback) {
+  return await client.get(key)
+    .then(async results => {
+      if (results !== null) {
+        return JSON.parse(results);
+      } else {
+        results = await callback();
+        client.set(key, JSON.stringify(results), { EX: DEFAULT_EXPIRATION });
+        return results;
+      }
+    })
+}
 
 module.exports = router;
