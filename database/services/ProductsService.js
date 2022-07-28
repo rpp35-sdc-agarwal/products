741f07f90d4e54d2d59aac3e08ad5a03cdcc386b
Kevin On <idealistkevin@gmail.com>
2022-07-27 21:01:13 -0700

    Switching back from non-raw sequelize queries to compare speeds

--- database/services/ProductsService.js
+++ database/services/ProductsService.js
@@ -1,5 +1,64 @@
 let { Products, Related, Features } = require('../models.js');
 
+// module.exports = {
+//   getAllProducts: async () => {
+//     // let offset = Math.floor(Math.random() * (1000001 - 1) + 1);
+//     let offset = 1;
+//     return await Products.findAll({
+//       benchmark: true,
+//       logging: console.log,
+//       raw: true,
+//       nest: true,
+//       where: { id: [
+//         offset,
+//         offset + 1,
+//         offset + 2,
+//         offset + 3,
+//         offset + 4
+//       ]},
+//     });
+//   },
+//   getOneProduct: async (product_id) => {
+//     let product = await Products.findAll({
+//       benchmark: true,
+//       logging: console.log,
+//       raw: true,
+//       nest: true,
+//       where: { id: product_id },
+//       include: {
+//         model: Features
+//       }
+//     })
+//     .then(sameProducts => {
+//       let product = sameProducts[0];
+//       let features = [];
+//       sameProducts.forEach(product => {
+//         features.push(product.features);
+//       })
+//       product.features = features;
+//       return product;
+//     })
+//     product.features.forEach(feature => {
+//       if (feature.value === "null") {
+//         feature.value = null;
+//       }
+//     })
+//     console.log(product);
+//     return product;
+//   },
+//   getRelated: async (product_id) => {
+//     let results = await Related.findAll({
+//       benchmark: true,
+//       raw: true,
+//       nest: true,
+//       logging: console.log,
+//       attributes: [ 'related_product_id' ],
+//       where: { product_id }
+//     })
+//     return results.map(result => result['related_product_id']);
+//   }
+// }
+
 module.exports = {
   getAllProducts: async () => {
     // let offset = Math.floor(Math.random() * (1000001 - 1) + 1);
@@ -7,8 +66,6 @@ module.exports = {
     return await Products.findAll({
       benchmark: true,
       logging: console.log,
-      raw: true,
-      nest: true,
       where: { id: [
         offset,
         offset + 1,
@@ -22,39 +79,25 @@ module.exports = {
     let product = await Products.findAll({
       benchmark: true,
       logging: console.log,
-      raw: true,
-      nest: true,
       where: { id: product_id },
       include: {
         model: Features
       }
     })
-    .then(sameProducts => {
-      let product = sameProducts[0];
-      let features = [];
-      sameProducts.forEach(product => {
-        features.push(product.features);
-      })
-      product.features = features;
-      return product;
-    })
     product.features.forEach(feature => {
-      if (feature.value === "null") {
-        feature.value = null;
+      if (feature.dataValues.value === "null") {
+        feature.dataValues.value = null;
       }
     })
-    console.log(product);
     return product;
   },
   getRelated: async (product_id) => {
     let results = await Related.findAll({
       benchmark: true,
-      raw: true,
-      nest: true,
       logging: console.log,
       attributes: [ 'related_product_id' ],
       where: { product_id }
     })
-    return results.map(result => result['related_product_id']);
+    return results.map(result => result.dataValues['related_product_id']);
   }
 }
--- database/services/StylesService.js
+++ database/services/StylesService.js
@@ -1,5 +1,52 @@
 let { Styles, Photos, Skus } = require('../models.js');
 
+// module.exports = {
+//   getProductStyle: async (product_id) => {
+//     return await Styles.findAll({
+//       benchmark: true,
+//       logging: console.log,
+//       where: { product_id },
+//       raw: true,
+//       nest: true,
+//       include: [ Photos, Skus ]
+//     })
+//     .then(styles => {
+//       let transformedStyles = [];
+//       styles.forEach(style => {
+//         // if there exists the style_id in transformedStyles
+//         if (transformedStyles.some(tStyle => tStyle.style_id === style.style_id)) {
+//           // find the index of that style in the transformedStyles
+//           let index = transformedStyles.findIndex(object => { return object.style_id === style.style_id });
+//           // append the sku into the transformedStyles
+//           transformedStyles[index].skus.push(style.skus);
+//         } else {
+//           // if not found, set up the photos and skus as arrays and push the style into the transformedStyles
+//           style.skus = [style.skus];
+//           style.photos = [style.photos];
+//           transformedStyles.push(style);
+//         }
+//       })
+//       return transformedStyles
+//     })
+//     .then(styles => {
+//       styles.forEach(style => {
+//         if (style.sale_price === 'null') {
+//           style.sale_price = null;
+//         }
+//         let skus = {};
+//         style.skus.forEach(sku => {
+//           skus[sku.id] = {
+//             quantity: sku.quantity,
+//             size: sku.size
+//           }
+//         })
+//         style.skus = skus;
+//       })
+//       return styles;
+//     })
+//   }
+// }
+
 module.exports = {
   getProductStyle: async (product_id) => {
     return await Styles.findAll({
